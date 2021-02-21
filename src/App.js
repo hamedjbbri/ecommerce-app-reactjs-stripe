a401ef1226f7d8d516eb0d99c75600a92726089c
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-02-21 11:04:58 -0800

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import Products from './components/Products/Products';
+ 
+import { Products, Navbar } from './components';
 
 const App = () => {
     return (
         <div>
+            <Navbar />
             <Products />
         </div>
     )
 }
 
-export default App
+export default App;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,17 +2,19 @@
  import { Grid } from '@material-ui/core';
 
  import Product from './Product/Product'
-
- import useStyles from './Product/styles';
+ import useStyles from './styles';
 
  const products = [
      { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://images.unsplash.com/photo-1578116922645-3976907a7671?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1000&q=80'},
-     { id: 2, name: 'Table', description: 'Living room table.', price:'$15', image: 'http://clipart-library.com/new_gallery/287-2878325_glass-furniture-png-photo-elise-coffee-table.png'},
+     { id: 2, name: 'Table', description: 'Living room table.', price:'$15', image: 'https://pngimg.com/uploads/table/table_PNG7005.png'},
  ];
 
  const Products = () => {
+
+     const classes = useStyles();
      return( 
-    <main>
+    <main className={classes.content}>
+        <div className={classes.toolbar} /> 
         <Grid container justify="center" spacing={4} >
            {products.map((product) => (
                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3} >
